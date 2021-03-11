70bff9b00e7e72a25193c9cf3519d43dce032d11
Jakub Míšek <jmisek@gmail.com>
2021-03-12 00:01:06 +0100

    updated generated vs changelog
    supports categories

--- content/vs/convert.php
+++ content/vs/convert.php
@@ -15,9 +15,27 @@ foreach ($xml->release as $r)
     $date = DateStr($r['date']);
     echo "## $r[version] ($date) $r[tags]", PHP_EOL, PHP_EOL;
 
+    $bycategory = ["" => []]; // set of notes grouped by category, "" is always first
+
     foreach ($r->note as $n)
     {
-        echo "- $n", PHP_EOL;
+        $category = (string)$n['category'];
+        if (empty($category))
+            $category = "";
+
+        $list = &$bycategory[$category];
+        $list[] = $n;
+    }
+
+    foreach ($bycategory as $category => $list) {
+        if (empty($list))
+            continue;
+
+        if (!empty($category))
+            echo PHP_EOL, "### $category", PHP_EOL, PHP_EOL;
+
+        foreach ($list as $line)
+            echo "- $line", PHP_EOL;
     }
 
     echo PHP_EOL;
