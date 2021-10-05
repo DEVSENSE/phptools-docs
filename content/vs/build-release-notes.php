8d33d3fb2b63793ee8bd1fef89baf2f06d6b0ecb
Jakub Míšek <jmisek@gmail.com>
2021-10-05 18:23:52 +0200

    release notes agregates minor versions

--- content/vs/build-release-notes.php
+++ content/vs/build-release-notes.php
@@ -5,28 +5,32 @@ $content = file_get_contents('changelog.md');
 $res = preg_split('/(##\s+[0-9].*)/', $content, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
 
 $releases = [];
+$major = '';
 foreach ($res as $element) 
 {
     if (preg_match_all('/#+\s+([0-9\.]+)\s*(?:\((.*)\))?\s?(\w+)?/', $element, $matches)) {
-        $release = [
-            'version' => $matches[1][0],
-            'date' => date("Ymd", strtotime($matches[2][0])),
-            'tags' => $matches[3][0] ?: 'stable',
-        ];
+        $version = $matches[1][0];
+        $rawdate = $matches[2][0];
+        $date = date("Y-m-d", strtotime($rawdate));
+        $tag = $matches[3][0] ?: 'stable';
+        $nums = preg_split('/\./', $version);
+        $major = "v$nums[0].$nums[1]";
+
+        $releases[$major] ??= ['date' => $date, 'versions' => '', 'text' => ''];
     }
     else {
-        $release['text'] = trim($element);
-        $releases[] = $release;
+        $releases[$major]['versions'] .= $version . ',';
+        $releases[$major]['text'] .= "# v$version\n> Date: $rawdate\n\n" . trim($element) . "\n\n";
     }
 }
 
-foreach ($releases as $r) {
+foreach ($releases as $v => $r) {
 
-    file_put_contents("releasenotes/v$r[version].md", <<<FILE
+    file_put_contents("releasenotes/$v.md", <<<FILE
 /*
-Title: PHP Tools v$r[version] Release Notes
-Tags: release notes,visual studio,$r[tags]
-Versions: $r[version]
+Title: PHP Tools $v Release Notes
+Tags: release notes,visual studio
+Versions: $r[versions]
 Date: $r[date]
 */
 
--- content/vs/changelog.md
+++ content/vs/changelog.md
@@ -1,7 +1,5 @@
 ## 1.56.14646 (October 3, 2021) stable
 
-> Release Notes at: [blog.devsense.com](https://blog.devsense.com/2021/php-tools-visual-studio-october-2021)
-
 > **Visual Studio 2019: 16.10+** required.
 
 > **Visual Studio 2022: Preview 4** required.
